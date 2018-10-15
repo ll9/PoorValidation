828bc9403318526edbbdb0280924c0508de8ae9e
ll9 <llwierer@web.de>
2018-10-15 20:02:26 +0200

    Model

---
+++ ColumnNoDuplicates/Models/ColumnModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColumnNoDuplicates.Models
+{
+    public enum DataType
+    {
+        Integer,
+        Boolean,
+        Double,
+        DateTime
+    }
+
+    class ColumnModel
+    {
+        public string Name { get; set; }
+        public DataType DataType { get; set; }
+    }
+}
---
+++ ColumnNoDuplicates/Views/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ColumnNoDuplicates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.columnModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.columnModelDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.columnModelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnModelDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // columnModelBindingSource
+            // 
+            this.columnModelBindingSource.DataSource = typeof(ColumnNoDuplicates.Models.ColumnModel);
+            // 
+            // columnModelDataGridView
+            // 
+            this.columnModelDataGridView.AutoGenerateColumns = false;
+            this.columnModelDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.columnModelDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.columnModelDataGridView.DataSource = this.columnModelBindingSource;
+            this.columnModelDataGridView.Location = new System.Drawing.Point(44, 86);
+            this.columnModelDataGridView.Name = "columnModelDataGridView";
+            this.columnModelDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.columnModelDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "DataType";
+            this.dataGridViewTextBoxColumn2.HeaderText = "DataType";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(44, 34);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 2;
+            this.NameLabel.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 380);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.columnModelDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.columnModelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnModelDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource columnModelBindingSource;
+        private System.Windows.Forms.DataGridView columnModelDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
